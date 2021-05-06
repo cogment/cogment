3b3c288fedc606eecbd897dd8d5eff4e0a06a24f
Clodéric Mars <cloderic@ai-r.com>
2021-05-06 15:16:07 -0400

    Update licenses pre-1.0

--- deployment/deployment_test.go
+++ deployment/deployment_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Artificial Intelligence Redefined <dev+cogment@ai-r.com>
+// Copyright 2021 Artificial Intelligence Redefined <dev+cogment@ai-r.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
